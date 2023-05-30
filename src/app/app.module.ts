72bd985191ff15a54f3a9003b013da2c2f08a25c
MelckTeyllor <melcksedeck123@gmail.com>
2023-05-29 22:28:50 -0300

    removendo importação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AplicacaoTesteComponent } from './core/aplicacao-teste/aplicacao-teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AplicacaoTesteComponent
   ],
   imports: [
     BrowserModule,
